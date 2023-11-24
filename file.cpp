a50bf5d45f2077decc59cffa7fcc464313be98d9
Muhammad Bilal <115155492+Apexvirus@users.noreply.github.com>
2023-11-24 16:35:23 +0500

    file class partial implementation

---
+++ file.cpp
@@ -0,0 +1,19 @@
+#include "file.h"
+
+    
+
+File::File(const std::string &n){
+
+}
+std::string File::getContent() const{
+    return content;
+}
+void File::setContent(const std::string &newContent){
+    
+}
+std::string File::getName() const{
+    return name;
+}
+time_t  File::getCreationDate() const{
+    return creationDate;
+}
